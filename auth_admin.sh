8b6e34b047cd76cfce335c058b701cdfa4fd5bfd
Jason Corbett <jasoncorbett@gmail.com>
2017-06-22 13:27:44 -0600

    changing default password of admin

--- auth_admin.sh
+++ auth_admin.sh
@@ -1,2 +1,2 @@
 #!/opt/local/bin/bash
-curl -s -X POST http://127.0.0.1:8080/auth/realms/slick/protocol/openid-connect/token  -H "Content-Type: application/x-www-form-urlencoded" -d "username=admin" -d 'password=password100' -d 'grant_type=password' -d 'client_id=slick-web' |json .access_token > auth/admin
+curl -s -X POST http://127.0.0.1:8080/auth/realms/master/protocol/openid-connect/token  -H "Content-Type: application/x-www-form-urlencoded" -d "username=admin" -d 'password=f00b@rIt' -d 'grant_type=password' -d 'client_id=slick-web' |json .access_token > auth/admin
